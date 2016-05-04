3ce3f355ec15f237df4fdebb1b520d1ad71eb801
Sahil Gupta <sahil@thetrackr.com>
2016-05-04 00:07:45 -0700

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogInRor::Application.config.secret_key_base = '32cf72033362b121407bc77e5b4e18933c881512fcaed81c09cdbf0a0ba4f2431195facf0a2e484a68e6fc8af936e10f9e6122a9b7a144aee9aea0b891e5d3ae'
